283179d9f46c9997361533c2c629e1716494f29e
Daniel Engelson <danen038@student.liu.se>
2021-02-12 11:48:11 +0100

    updated run

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --network="host" --dns 8.8.8.8 --privileged -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/daniel/exjobb:/ws -it --rm  danneengelson/exjobb
+docker run --network="host" --dns 8.8.8.8 --privileged -e QT_X11_NO_MITSHM=1 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/daniel/exjobb:/ws -it --rm  danneengelson/exjobb
