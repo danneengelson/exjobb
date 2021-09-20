72b912e9308057640aeb06c3b7beb2b1af6ac464
Daniel Engelson <danen038@student.liu.se>
2021-09-20 20:13:06 +0200

    launch

---
+++ src/exjobb/launch/video_show_terrain_assessment.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='exjobb',
+            executable='video_show_terrain_assessment',
+            parameters=[
+                {"use_sim_time": False},
+            ]
+        ),    
+        Node(
+            package='rviz2',
+            executable='rviz2',
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=["0", "0", "0", "0", "0", "0", "map", "my_frame"]
+        ),
+        ])
